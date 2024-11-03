da3e404152df18500aa35440c1832696aeee4754
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2024-11-03 01:28:46 +0100

    refactor: amélioration de la configuration du store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "../features/taskSlice";
 
-// Configuration du store Redux avec le reducer des tâches
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
   },
 });
 
-// Type pour accéder au state global de l'application
 export type RootState = ReturnType<typeof store.getState>;
-
-// Type pour le dispatch des actions avec TypeScript
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
